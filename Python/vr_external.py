e8e0a1aba654e743f4c541bd00162fe254233549
RaspberyPicardBox <raspberrypicardbox@gmail.com>
2022-05-11 18:58:19 +0100

    Added new VR external mode

--- Python/vr_external.py
+++ Python/vr_external.py
@@ -13,77 +13,67 @@ BUTTONS = 6
 ANALOG_AXIS = 3
 
 GRIP = (0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+BUTTON_X = (0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
 
 def eular_angle(axis, quart):
     # From Shital Shah on https://stackoverflow.com/questions/5782658/extracting-yaw-from-a-quaternion
     if axis == 'roll':
         angle = math.atan2(2.0 * (quart[3] * quart[2] + quart[0] * quart[1]), 1.0 - 2.0 * (quart[1] * quart[1] + quart[2] * quart[2]))
-        # angle = p.getEulerFromQuaternion(quart)[0]
     elif axis == 'pitch':
         angle = math.asin(2.0 * (quart[2] * quart[0] - quart[3] * quart[1]))
-        # angle = p.getEulerFromQuaternion(quart)[1]
     elif axis == 'yaw':
         angle = math.atan2(2.0 * (quart[3] * quart[0] + quart[1] * quart[2]), - 1.0 + 2.0 * (quart[0] * quart[0] + quart[1] * quart[1]))
-        # angle = p.getEulerFromQuaternion(quart)[2]
     else:
         angle = None
-
     return angle
-
-
-def rotate_head(pepper):
-    while True:
-        HMDEvents = p.getVREvents(2)
-        for e in HMDEvents:
-            pepper.moveTo(pepper.getPosition()[0], pepper.getPosition()[1], eular_angle('roll', e[ORIENTATION]) + 1.5708, speed=0.1, frame=1, _async=True)
 
 
 def move_left_arm(pepper):
     global LendEffectorLinkIndex
     while True:
         events = p.getVREvents()
-        try:
-            if events[0]:
-                ik = p.calculateInverseKinematics(bodyUniqueId, LendEffectorLinkIndex, events[0][POSITION])
-                angles = list(ik)[:-3]
-                angles.remove(angles[9])
-                angles.remove(angles[28])
-                angles[0:3] = [0, 0, 0]
-                pepper.setAngles(joints, angles, 1)
-        except IndexError:
-            pass
+        for e in events:
+            if e[CONTROLLER_ID] == 1:
+                if e[BUTTONS] == GRIP and p.VR_BUTTON_IS_DOWN:
+                    pepper.setAngles('LWristYaw', -eular_angle('yaw', e[ORIENTATION]) - 3.14, 1)
+                    ik = p.calculateInverseKinematics(bodyUniqueId, LendEffectorLinkIndex, e[POSITION])
+                    angles = list(ik)[:-3]
+                    angles.remove(angles[9])
+                    angles.remove(angles[28])
+                    angles[0:3] = [0, 0, 0]
+                    pepper.setAngles(joints, angles, 1)
 
 
 def move_right_arm(pepper):
     global RendEffectorLinkIndex
     while True:
         events = p.getVREvents()
-        try:
-            if events[1]:
-                ik = p.calculateInverseKinematics(bodyUniqueId, RendEffectorLinkIndex, events[1][POSITION])
-                angles = list(ik)[:-3]
-                angles.remove(angles[9])
-                angles.remove(angles[28])
-                angles[0:3] = [0, 0, 0]
-                pepper.setAngles(joints, angles, 1)
-        except IndexError:
-            pass
+        for e in events:
+            if e[CONTROLLER_ID] == 2:
+                if e[BUTTONS] == GRIP and p.VR_BUTTON_IS_DOWN:
+                    pepper.setAngles('RWristYaw', -eular_angle('yaw', e[ORIENTATION]) + 3.14, 1)
+                    ik = p.calculateInverseKinematics(bodyUniqueId, RendEffectorLinkIndex, e[POSITION])
+                    angles = list(ik)[:-3]
+                    angles.remove(angles[9])
+                    angles.remove(angles[28])
+                    angles[0:3] = [0, 0, 0]
+                    pepper.setAngles(joints, angles, 1)
 
 
-def move_thread(pepper):
-    global head_thread
+def move_pepper(pepper):
     while True:
         events = p.getVREvents()
+
         for e in events:
             if e[CONTROLLER_ID] == 1:
-                pepper.setAngles('LWristYaw', -eular_angle('yaw', e[ORIENTATION])-3.14, 1)
-                if e[BUTTONS] == GRIP and p.VR_BUTTON_IS_DOWN:
-                    pepper.moveTo(1, 0, 0, speed=5, _async=True)
-            else:
-                pepper.setAngles('RWristYaw', -eular_angle('yaw', e[ORIENTATION])+3.14, 1)
-                if e[BUTTONS] == GRIP and p.VR_BUTTON_IS_DOWN:
-                    pepper.moveTo(-1, 0, 0, speed=5, _async=True)
+                if e[BUTTONS] == BUTTON_X and p.VR_BUTTON_IS_DOWN:
+                    # pepper.move(1, 0, eular_angle("yaw", e[ORIENTATION]))
+                    print("X button pressed")
+                    pepper.move(0.5, 0, 0)
+                if not p.VR_BUTTON_IS_DOWN:
+                    print("Button not pressed")
+                    pepper.stopMove()
 
 
 if __name__ == "__main__":
@@ -97,8 +87,6 @@ if __name__ == "__main__":
         spawn_ground_plane=True)
     pepper.goToPosture('Stand', 1)
 
-    pybullet.setVRCameraState(trackObject=pepper.getRobotModel(), trackObjectFlag=1)
-
     bodyUniqueId = pepper.getRobotModel()
     joints = list(pepper.joint_dict.keys())
     joints.remove('LWristYaw')
@@ -107,14 +95,12 @@ if __name__ == "__main__":
     LendEffectorLinkIndex = pepper.link_dict["l_hand"].getIndex()
     RendEffectorLinkIndex = pepper.link_dict["r_hand"].getIndex()
 
-    head_thread = threading.Thread(target=rotate_head, args=(pepper,), daemon=True)
     l_hand_thread = threading.Thread(target=move_left_arm, args=(pepper,), daemon=True)
     r_hand_thread = threading.Thread(target=move_right_arm, args=(pepper,), daemon=True)
-    movement_thread = threading.Thread(target=move_thread, args=(pepper,), daemon=True)
-    head_thread.start()
+    move_thread = threading.Thread(target=move_pepper, args=(pepper,), daemon=True)
     l_hand_thread.start()
     r_hand_thread.start()
-    movement_thread.start()
+    move_thread.start()
 
     try:
         while True:
